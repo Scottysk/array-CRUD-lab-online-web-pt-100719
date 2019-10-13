109500ab228e5e0a15fbae7131ae987e5c8e4d75
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-13 21:22:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  LOTR_races = ["Human", "Elf", "Dwarf", "Ent"]
-  LOTR_races << "Balrog"
+  ["Human", "Elf", "Dwarf", "Ent"]
+  "Balrog"
 end
 
 def add_element_to_start_of_array(array, element)
